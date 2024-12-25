f5aa447875caca1c1912a9909c8fe10366ee0092
lixixi89055465 <nanji1850094299@outlook.com>
2024-12-25 23:30:19 +0800

    init

--- test01/testTensorboard03.py
+++ test01/testTensorboard03.py
@@ -10,6 +10,7 @@ import torch
 # import torchvision.utils as utils
 import numpy as np
 import torchvision.models as models
+import torchvision.utils
 from torchvision import datasets
 from tensorboardX import SummaryWriter
 
@@ -23,18 +24,39 @@ for n_iter in range(100):
     # data grouping by 'slash'
     writer.add_scalar('data/scalar1', dummy_s1[0], n_iter)
     writer.add_scalar('data/scalar2', dummy_s2[0], n_iter)
-    writer.add_scalar('data/scalar_group', {
-        'xsinx': n_iter * np.sin(n_iter),
-        'xcosx': n_iter * np.cos(n_iter),
-        'arctanx': np.arctan(n_iter)
-    }, n_iter)
-    writer.add_scalar('data/scalar_group', {
+    writer.add_scalars('data/scalar_group',
+                       {'xsinx': n_iter * np.sin(n_iter),
+                        'xcosx': n_iter * np.cos(n_iter),
+                        'arctanx': np.arctan(n_iter)}, n_iter)
+    writer.add_scalars('data/scalar_group', {
         'xsinx': n_iter * np.sin(n_iter),
         'xcosx': n_iter * np.cos(n_iter),
         'arctanx': np.arctan(n_iter)
     }, n_iter)
     dummy_img = torch.rand(32, 3, 64, 64)
     if n_iter % 10 == 0:
-        x = torch.utils.make_grid(dummy_img, normalize=True, scale_each=True)
+        # x = torch.utils.make_grid(dummy_img, normalize=True, scale_each=True)
+        x = torchvision.utils.make_grid(dummy_img, normalize=True, scale_each=True)
         writer.add_image('Image', x, n_iter)
         dummy_audio = torch.zeros(sample_rate * 2)
+        for i in range(x.size(0)):
+            dummy_audio[i] = np.cos(freqs[n_iter // 10] * np.pi * float(i) / float(sample_rate))
+        writer.add_audio('myAudio', dummy_audio, n_iter, sample_rate=sample_rate)
+        writer.add_text('Text', 'texdt logged at step:' + str(n_iter), n_iter)
+        for name, param in resnet18.named_parameters():
+            writer.add_histogram(name, param.clone().cpu().data.numpy(), n_iter)
+        # need tensorboard 0.4RC or later
+        writer.add_pr_curve('xoxo', np.random.randint(2, size=100), np.random.rand(100), n_iter)
+dataset = datasets.MNIST('mnist', train=False, download=True)
+# images = dataset.test_data[:100].float()
+# label = dataset.test_labels[:100]
+images = dataset.data[:100].float()
+label = dataset.targets[:100]
+
+features = images.view(100, 784)
+writer.add_embedding(features, metadata=label, label_img=images.unsqueeze(1))
+# dxport scalar data to JSON for external processing
+writer.export_scalars_to_json('./all_scalars.json')
+writer.close()
+# tensorboard --logdir=./path/to/the/folder --port 8123
+# 重新确定torch和 tensorboard 版本
